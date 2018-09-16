cccf5c36da8d54aaaf42eabb220a390f9431e516
LeonardoGrotti <leo.grotti1@gmail.com>
2018-09-16 20:48:14 -0300

    adicionando uma file

---
+++ modelo_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Bar {
+  final int id;
+  final String name;
+  final String image;
+  final String worktime;
+  final String description;
+  final String address;
+  final distance;
+  final List<Map<String, String>> drinks;
+  //final List drinks;
+
+  Bar(
+// apenas um teste para aprender a commitar
+
+      {@required this.id,
+      @required this.name,
+      @required this.image,
+      @required this.worktime,
+      @required this.description,
+      @required this.address,
+      @required this.distance,
+      @required this.drinks});
+}
+print('oi')
